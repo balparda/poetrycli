790d06f10023c84f5dcb90188fc369c53bf9f46b
Daniel Balparda <balparda@gmail.com>
2026-01-17 02:11:48 +0000

    __main__

---
+++ src/mycli/__main__.py
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: Copyright 2026 Daniel Balparda <balparda@github.com>
+# SPDX-License-Identifier: Apache-2.0
+"""Main stub."""
+
+from __future__ import annotations
+
+from .cli import app
+
+if __name__ == '__main__':
+  app()
